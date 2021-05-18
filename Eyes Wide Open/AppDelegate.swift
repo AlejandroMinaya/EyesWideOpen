09417af971abf1bbcabe6efe537f117df0d8cb32
Juan Alcántara <j.alejandrominaya@gmail.com>
2021-05-18 15:26:29 -0500

    feat: Add choose file button

--- Eyes Wide Open/AppDelegate.swift
+++ Eyes Wide Open/AppDelegate.swift
@@ -11,10 +11,24 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
-
-
+    private var filePath: String!
+    
+    @IBAction func chooseFile(button: NSObject) {
+        let dialog = NSOpenPanel();
+        dialog.allowedFileTypes = ["jpg","jpeg"]
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url
+            
+            if (result != nil) {
+                filePath = result!.path
+            }
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
